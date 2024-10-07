13d8df9e4343ec53f7b92ab3858828ac3cd5f87c
Mihai <mihai.hulea@aut.utcluj.ro>
2024-10-07 17:55:50 +0200

    Update demo.

--- laborator_1/src/main/java/ssatr/lab1/exercise1/Exercise1.java
+++ laborator_1/src/main/java/ssatr/lab1/exercise1/Exercise1.java
@@ -6,9 +6,9 @@ public class Exercise1 {
         Address a1 = new Address("asdas", "123");
         System.out.println(a1);
         
-        Customer c1 = new Customer(new Address("aaa","bb"), "id1");
+        Customer c2 = new Customer(new Address("aaa","bb"), "id1");
         
-        System.out.println(c1);
+        System.out.println(c2);
         
     }
 }
